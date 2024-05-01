4d40371bfd914a4ad0dac85c7bc251be9ab5d9c4
unjerry <2351688512@qq.com>
2024-05-01 16:38:06 +0800

    one_trace_evit_the_circle

---
+++ test1.py
@@ -0,0 +1,136 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+
+rt = 10
+
+th = torch.linspace(0, torch.pi * 2, 100)
+uni = torch.tensor([5.0 * rt, 0.0])
+matt = torch.stack(
+    [
+        torch.stack([torch.cos(th), -torch.sin(th)], dim=1),
+        torch.stack([torch.sin(th), torch.cos(th)], dim=1),
+    ],
+    dim=1,
+)
+Pp = torch.einsum("nij,j->ni", matt, uni)
+plt.scatter(Pp[:, 0], Pp[:, 1])
+# plt.show()
+
+
+class M(nn.Module):
+    def __init__(self, n, p0, dl, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.vec = nn.Parameter(torch.zeros([n], requires_grad=True))
+        self.sig = nn.Sigmoid()
+        self.P0 = nn.Parameter(p0, requires_grad=False)
+        self.dl = nn.Parameter(torch.tensor(dl), requires_grad=False)
+        self.theh = nn.Parameter(torch.tensor([1.0]), requires_grad=True)
+        self.unit = nn.Parameter(torch.tensor([1.0, 0]), requires_grad=False)
+
+    def forward(self):
+        thetas = (2 * self.sig(self.vec) - 1) * torch.pi / 3
+        # print("pre", pre)
+        mat0 = torch.stack(
+            [
+                torch.concatenate([torch.cos(self.theh), -torch.sin(self.theh)], dim=0),
+                torch.concatenate([torch.sin(self.theh), torch.cos(self.theh)], dim=0),
+            ],
+            dim=0,
+        )
+        # print(mat0.shape)
+        unit = torch.einsum("ij,j->i", mat0, self.unit)
+        pre = self.dl * unit
+        mat = torch.stack(
+            [
+                torch.stack([torch.cos(thetas), -torch.sin(thetas)], dim=1),
+                torch.stack([torch.sin(thetas), torch.cos(thetas)], dim=1),
+            ],
+            dim=1,
+        )
+        # print(mat)
+        P = self.P0
+        for i in range(thetas.shape[0]):
+            # print(thetas[i], mat[i])
+            pre = mat[i] @ pre
+            P = torch.concatenate((P, (P[-1, :] + pre).view(1, -1)), dim=0)
+        # print(P)
+        return P
+
+
+def cost_function(x):
+    end = torch.tensor([35.0 * rt, 0.0], device="cpu")
+    circ = torch.tensor([0.0, 0.0], device="cpu")
+    dif = x[-1] - end
+    dist = torch.einsum("i,i->", dif, dif)
+    # print(dist)
+    dd = torch.einsum("i,i->", dif, dif)
+    # / (
+    #     torch.sqrt(
+    #         torch.einsum("i,i->", torch.tensor([5500, 0]), torch.tensor([5500, 0]))
+    #     )
+    # )
+    ans = dd
+    # ans = 0
+    for p in x:
+        # print(p)
+        # print(
+        #     torch.exp(
+        #         -5 * (torch.einsum("i,i->", (p - circ) / 500, (p - circ) / 500))
+        #     ).tolist()
+        # )
+        ans += dist * torch.log(1.0 + torch.exp((5.0 * rt - torch.dist(p, circ))))
+
+    return ans
+
+
+p0 = torch.tensor([[-10.0 * rt, 0.0]], device="cpu")
+len = 45 * rt
+n = 66
+model = M(n, p0, 0.3 * rt)
+model.to("cpu")
+opt = torch.optim.Adam(model.parameters())
+
+z = model().detach().cpu()
+print(z)
+# plt.scatter(z[:, 0], z[:, 1])
+# plt.gca().set_aspect(1.0)
+# plt.savefig("figs/tmp1.png")
+
+preloss = torch.tensor(0.0)
+for i in range(100000):
+    opt.zero_grad()
+    z = model()
+    loss = cost_function(z)
+    loss.backward()
+    opt.step()
+    torch.set_printoptions(precision=4, linewidth=1000)
+    print(
+        f"{i}\t{z[-1]}\t{loss.tolist():.4f}\t{preloss.tolist():.4f}\t{(torch.abs(preloss-loss)).tolist():.4f}"
+    )
+    # if torch.abs(preloss - loss).tolist() < 1e-5 or loss.tolist() < 1e-4:
+    #     break
+    preloss = loss
+    if (i + 1) % 1000 == 0:
+        z = model().detach().cpu()
+        plt.gca().set_aspect(1.0)
+        plt.xlim((-15 * rt, 40 * rt))
+        plt.ylim((-55 * rt / 2, 55 * rt / 2))
+        plt.scatter(z[:, 0], z[:, 1])
+        plt.savefig(f"figs/tmp_{i}.png")
+        # plt.show()
+
+z = model().detach().cpu()
+print(z, z.device, sep="\n")
+
+import matplotlib.pyplot as plt
+
+plt.scatter(z[:, 0], z[:, 1], alpha=1)
+plt.gca().set_aspect(1.0)
+plt.xlim((-15 * rt, 40 * rt))
+plt.ylim((-55 * rt / 2, 55 * rt / 2))
+plt.savefig("figs/tmp.png")
+plt.show()
